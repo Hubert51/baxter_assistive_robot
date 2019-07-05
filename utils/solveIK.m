4fc9eb1c7b19f48bcd29724fca57829c2a39bcfa
Hubert51 <gengr@rpi.edu>
2019-07-04 23:50:12 -0400

    make the function cleaner

--- utils/solveIK.m
+++ utils/solveIK.m
@@ -1,48 +1,43 @@
 function [ my_qs ] = solveIK(robotArm, pos, ori, side)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-    torlence = 0.005;
-    offset = 0;
-    r= 0.36;
-    deltaTheta = 10/180*pi;
-    pause(1.5);
-    wrenches = robotArm.endeffector_wrenches;
-    wrenches = wrenches(10:12)
-
-    joint_pos = robotArm.joint_positions;
-    if strcmp(side, 'right')
-        joint_pos = joint_pos(8:14);
-    end
-    
+torlence = 0.005;
+for i=1:8
     my_qs = robotArm.solveIKfast(pos, ori, side);
     if  isempty(my_qs)
         joint_diff = [2];
+        pos = pos +torlence * (rand(3,1) - 0.5);
+        if i >= 5
+            ori = ori +torlence * (rand(4,1) - 0.5);
+        end
     else
-        joint_diff = my_qs - joint_pos;
+        break
     end
-    
-    i = 1;
-    while ~isempty(find( abs(joint_diff) > 1.5, 1))
-        %pos = pos +torlence * (rand(3,1) - 0.5);
-        % ori = ori +torlence * (rand(4,1) - 0.5);
-        my_qs = robotArm.solveIKfast(pos, ori, side);
-        
-        if  isempty(my_qs)
-            joint_diff = [1];
-        else
-            joint_diff = my_qs - joint_pos;
-        end
-
-        disp 'recalculate trajectory';
-        disp(i);
-        i = i+1;
-        
-        if i > 20
-            my_qs = [];
-            break;
-        end
-    end
-
-
 end
+
+%         i = 1;
+%         while ~isempty(find( abs(joint_diff) > 1.5, 1))
+%             %pos = pos +torlence * (rand(3,1) - 0.5);
+%             % ori = ori +torlence * (rand(4,1) - 0.5);
+%             my_qs = robotArm.solveIKfast(pos, ori, side);
+% 
+%             if  isempty(my_qs)
+%                 joint_diff = [1];
+%             else
+%                 joint_diff = my_qs - joint_pos;
+%             end
+% 
+%             disp 'recalculate trajectory';
+%             disp(i);
+%             i = i+1;
+% 
+%             if i > 20
+%                 my_qs = [];
+%                 break;
+%             end
+%         end
+%     end
+
+
+% end
 
