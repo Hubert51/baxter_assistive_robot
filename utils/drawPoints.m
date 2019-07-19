d0cdcb21b9ec3f4aa88937b8910e9f09595527c3
Hubert51 <gengr@rpi.edu>
2019-07-19 13:41:19 -0400

    function to draw the pose of the end-effector

--- utils/drawPoints.m
+++ utils/drawPoints.m
@@ -6,13 +6,13 @@ function [ output_args ] = drawPoints( RoadMap )
     z = [];
     for key=RoadMap.keys
         point = RoadMap(char(key));
-        quiver3(point.pos(1), point.pos(2), point.pos(3), 0, 0.1, 0)
+        % quiver3(point.pos(1), point.pos(2), point.pos(3), 0, 0.1, 0)
         x = [x, point.pos(1)];
         y = [y, point.pos(2)];
         z = [z, point.pos(3)];    
     end
-%     scatter3(y, x, z);
-    quiver3(x, y, z, 0, 0.1, 0)
+    scatter3(y, x, z);
+%     quiver3(x, y, z, 0, 0.1, 0)
     xlabel('y')
     ylabel('x')
     zlabel('z')
